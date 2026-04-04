2d034d3c3a05d292e59c305f40db64c1e178e74a
Manohar Solanki <119101769+solankimanohar@users.noreply.github.com>
2026-04-05 00:10:05 +0530

    Create Problem_007.cpp

---
+++ Problem_007.cpp
@@ -0,0 +1,29 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+bool isPrime(int n){
+    if(n <= 1) return false;
+    if(n <= 3) return true;
+    if(n % 2 == 0 || n % 3 == 0) return false;
+    for(int i = 5; i*i <= n;i += 6){
+        if(n % i == 0 || n % (i+2) == 0){
+            return false;
+        }
+    }
+    return true;
+}
+int main(){
+    int count = 0;
+    int n = 2;
+    while(true){
+        if(isPrime(n)){
+            count++;
+            if(count == 10001){
+                cout<<n;
+                break;
+            }
+        }
+        n++;
+    }
+    return 0;
+}
